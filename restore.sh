6c573aba3b1e6442a05552278fc501e9161ccd58
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-05-16 18:01:40 +0530

    add .screenrc

--- restore.sh
+++ restore.sh
@@ -1,6 +1,7 @@
 ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.zshenv ~/.zshenv
 ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.screenrc ~/.screenrc
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s `pwd`/.gitignore ~/.gitignore
