a98eb6277d904682a87ca479905fcb67f8a48304
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-05-30 18:09:50 +0530

    add .ssh/config

--- restore.sh
+++ restore.sh
@@ -5,3 +5,6 @@ ln -s `pwd`/.screenrc ~/.screenrc
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
 ln -s `pwd`/.gitignore ~/.gitignore
+
+mkdir -p ~/.ssh
+ln -s `pwd`/.ssh/config ~/.ssh/config
