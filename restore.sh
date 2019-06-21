8e56fbb0d202280742f0b9d09adab0535abf3b5a
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-06-21 17:13:20 +0530

    bash: case-insensitive autocomplete

--- restore.sh
+++ restore.sh
@@ -1,6 +1,7 @@
 ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.zshenv ~/.zshenv
 ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.inputrc ~/.inputrc
 ln -s `pwd`/.screenrc ~/.screenrc
 ln -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -s `pwd`/.gitconfig ~/.gitconfig
