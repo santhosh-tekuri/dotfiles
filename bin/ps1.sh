146857f7581ea502492bee27923efbc6b614bc53
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-06-21 20:48:09 +0530

    git-ps1 executable extracted

--- bin/ps1.sh
+++ bin/ps1.sh
@@ -1,34 +1,9 @@
-function git_prompt {
-    curr_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null);
-    if [ $? -eq 0 ]; then
-        curr_remote=$(git config branch.$curr_branch.remote);
-        if git config branch.$curr_branch.merge > /dev/null; then
-            curr_merge_branch=$(git config branch.$curr_branch.merge | cut -d / -f 3);
-            ahead=$(git rev-list --left-only --count $curr_branch...$curr_remote/$curr_merge_branch);
-            behind=$(git rev-list --right-only --count $curr_branch...$curr_remote/$curr_merge_branch);
-
-            if [ $ahead -ne 0 ]; then
-                echo -n "+$ahead "
-            fi
-            if [ $behind -ne 0 ]; then
-                echo -n "-$behind "
-            fi
-        fi
-        echo -n $curr_branch
-        if [ -z "$(git status --porcelain)" ]; then
-            echo -n " "
-        else
-            echo -n "*"
-        fi
-    fi
-}
-
 # reevaluate the prompt expression each time a prompt is displayed
 setopt prompt_subst
 
 # remove any right prompt from display when accepting a command line
 setopt transientrprompt 
 
-PS1="%(?..%F{red})\$(git_prompt)➤ %f "
+PS1="%(?..%F{red})\$(git-ps1)➤ %f "
 RPS1='%K{blue}[%~]%k'
 zle_highlight=(default:fg=yellow,bold)
