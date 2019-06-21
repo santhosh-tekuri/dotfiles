91bbed0716626184241d986acdb7cedad04a0370
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-06-21 21:12:12 +0530

    create symblink for .bashrc

--- restore.sh
+++ restore.sh
@@ -1,10 +1,11 @@
-ln -s `pwd`/.zshrc ~/.zshrc
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.inputrc ~/.inputrc
-ln -s `pwd`/.screenrc ~/.screenrc
-ln -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/.gitconfig ~/.gitconfig
-ln -s `pwd`/.gitignore ~/.gitignore
+ln -s `pwd`/.zshrc ~
+ln -s `pwd`/.vimrc ~
+ln -s `pwd`/.inputrc ~
+ln -s `pwd`/.screenrc ~
+ln -s `pwd`/.tmux.conf ~
+ln -s `pwd`/.gitconfig ~
+ln -s `pwd`/.gitignore ~
+ln -s `pwd`/.bashrc ~
 
 mkdir -p ~/.ssh
 ln -s `pwd`/.ssh/config ~/.ssh/config
