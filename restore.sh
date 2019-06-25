5e12fab5918e38cd2ad193720d0265f4e31606df
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2019-06-25 21:25:07 +0530

    source bashrc file

--- restore.sh
+++ restore.sh
@@ -5,7 +5,7 @@ ln -s `pwd`/.screenrc ~
 ln -s `pwd`/.tmux.conf ~
 ln -s `pwd`/.gitconfig ~
 ln -s `pwd`/.gitignore ~
-ln -s `pwd`/.bashrc ~
+echo source `pwd`/.bashrc >> ~/.bashrc
 
 mkdir -p ~/.ssh
 ln -s `pwd`/.ssh/config ~/.ssh/config
