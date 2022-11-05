9af4dc4240d2696b074b61bec990eb561e57b4fd
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2022-11-06 00:28:35 +0530

    install: use source in .screenrc

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
+echo source `pwd`/.bashrc >> ~/.bashrc
 echo source `pwd`/.zshrc >> ~/.zshrc
 echo source `pwd`/.vimrc >> ~/.vimrc
+echo source `pwd`/.screenrc >> ~/.screenrc
 ln -s `pwd`/.inputrc ~
-ln -s `pwd`/.screenrc ~
 ln -s `pwd`/.tmux.conf ~
 ln -s `pwd`/.gitconfig ~
-echo source `pwd`/.bashrc >> ~/.bashrc
 
 mkdir -p ~/.ssh
 echo Include `pwd`/.ssh/config >> ~/.ssh/config
