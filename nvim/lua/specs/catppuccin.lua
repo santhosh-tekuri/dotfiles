ca6a79ea053a401a9d4b444e882526adf8c1ca48
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-14 22:06:33 +0530

    nvim: override background colors

--- nvim/lua/specs/catppuccin.lua
+++ nvim/lua/specs/catppuccin.lua
@@ -3,6 +3,12 @@ local spec = { "catppuccin/nvim" }
 function spec.config()
   require("catppuccin").setup({
     flavour = "mocha",
+    color_overrides = {
+      mocha = {
+        base = "#2c2e34",
+        mantle = "#3b3e48",
+      }
+    }
   })
   vim.cmd "colorscheme catppuccin"
 end
