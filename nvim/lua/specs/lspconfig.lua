e4304e2c88bae49e210c7fb3f93a6ea7208669ab
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-15 15:41:57 +0530

    nvim: set diagnotic text

--- nvim/lua/specs/lspconfig.lua
+++ nvim/lua/specs/lspconfig.lua
@@ -33,6 +33,17 @@ function spec.config()
         lspconfig[server].setup(opts)
     end
 
+    -- set diagnotic text
+    local signs = {
+        { name = 'DiagnosticSignError', text = '' },
+        { name = 'DiagnosticSignWarn', text = '' },
+        { name = 'DiagnosticSignHint', text = '' },
+        { name = 'DiagnosticSignInfo', text = '' },
+    }
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+    end
+
 end
 
 return spec
