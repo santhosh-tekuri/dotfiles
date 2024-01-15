0a14164f30b5b7fb14cc00ad70395f20dfd28577
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-15 11:44:30 +0530

    cosmetic: rm trailing spaces

--- nvim/lua/specs/treesitter.lua
+++ nvim/lua/specs/treesitter.lua
@@ -1,10 +1,10 @@
-local spec = { 
+local spec = {
   "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
 }
 
 function spec.config()
   require'nvim-treesitter.configs'.setup {
-    ensure_installed = { 
+    ensure_installed = {
       "c", "lua", "vim", "vimdoc", "query", -- should always be installed
       "xml", "json", "yaml", "toml",        -- config related
       "go", "gomod", "gosum", "gowork",     -- golang related
