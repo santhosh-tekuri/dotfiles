79cd02c2e15aeaf7cf9272639bab1138a5c0e136
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-16 02:09:06 +0530

    nvim: add live search

--- nvim/lua/specs/fzf.lua
+++ nvim/lua/specs/fzf.lua
@@ -8,6 +8,7 @@ function spec.config()
 
     vim.keymap.set('n', ' f', fzf.files, { desc = "Open file picker" })
     vim.keymap.set('n', ' b', fzf.buffers, { desc = "Open buffer picker" })
+    vim.keymap.set('n', ' /', fzf.live_grep, { desc = "Global search in workspace folder" })
 end
 
 return spec
