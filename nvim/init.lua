4e01ecf7c0e7926c4efc1ee0573da9ada6f08842
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-16 17:08:59 +0530

    nvim: add goto bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,15 @@ vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.opt.signcolumn = "yes" -- always show signcolumn
 vim.opt.wrap = false
 
+-- goto motions
+vim.keymap.set('n', 'gl', '$', { desc = "Goto line end" })
+vim.keymap.set('n', 'gh', '0', { desc = "Goto line start" })
+vim.keymap.set('n', 'gs', '^', { desc = "Goto first non-blank in line" })
+vim.keymap.set('n', 'ge', 'G', { desc = "Goto last line" })
+vim.keymap.set('n', 'gt', 'H', { desc = "Goto window top" })
+vim.keymap.set('n', 'gc', 'M', { desc = "Goto window center" })
+vim.keymap.set('n', 'gb', 'L', { desc = "Goto window bottom" })
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
