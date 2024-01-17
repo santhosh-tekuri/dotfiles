ded8b0a3bdda5e835357c3dd6f00422f36a66439
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-17 17:55:49 +0530

    nvm/cmp: emulate intellij behavior

--- nvim/lua/specs/cmp.lua
+++ nvim/lua/specs/cmp.lua
@@ -23,7 +23,10 @@ function spec.config()
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+            -- emulate intellij behavior
+            ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+            ['<Tab>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
         }),
         sources = {
             { name = "nvim_lsp" },
