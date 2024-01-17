97e7bfafd25de292810a298756351a9afc5e8843
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-17 17:18:03 +0530

    nvim: format on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
-vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 4 -- no of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- no of spaces inserted for tab
+vim.opt.expandtab = true   -- convert tabs to spaces
+vim.opt.shiftwidth = 4     -- no of spaces inserted for each indentation
+vim.opt.tabstop = 4        -- no of spaces inserted for tab
 vim.opt.signcolumn = "yes" -- always show signcolumn
 vim.opt.wrap = false
 
@@ -13,13 +13,16 @@ vim.keymap.set('n', ' p', '"+p', { desc = "Paste clipboard after selection" })
 vim.keymap.set('n', ' P', '"+P', { desc = "Paste clipboard before selection" })
 
 -- goto motions
-vim.keymap.set({'n', 'v'}, 'gl', '$', { desc = "Goto line end" })
-vim.keymap.set({'n', 'v'}, 'gh', '0', { desc = "Goto line start" })
-vim.keymap.set({'n', 'v'}, 'gs', '^', { desc = "Goto first non-blank in line" })
-vim.keymap.set({'n', 'v'}, 'ge', 'G', { desc = "Goto last line" })
-vim.keymap.set({'n', 'v'}, 'gt', 'H', { desc = "Goto window top" })
-vim.keymap.set({'n', 'v'}, 'gc', 'M', { desc = "Goto window center" })
-vim.keymap.set({'n', 'v'}, 'gb', 'L', { desc = "Goto window bottom" })
+vim.keymap.set({ 'n', 'v' }, 'gl', '$', { desc = "Goto line end" })
+vim.keymap.set({ 'n', 'v' }, 'gh', '0', { desc = "Goto line start" })
+vim.keymap.set({ 'n', 'v' }, 'gs', '^', { desc = "Goto first non-blank in line" })
+vim.keymap.set({ 'n', 'v' }, 'ge', 'G', { desc = "Goto last line" })
+vim.keymap.set({ 'n', 'v' }, 'gt', 'H', { desc = "Goto window top" })
+vim.keymap.set({ 'n', 'v' }, 'gc', 'M', { desc = "Goto window center" })
+vim.keymap.set({ 'n', 'v' }, 'gb', 'L', { desc = "Goto window bottom" })
+
+-- lsp-format on save
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -49,4 +52,3 @@ require("lazy").setup({
 })
 
 vim.cmd.colorscheme "kanagawa-wave"
-
