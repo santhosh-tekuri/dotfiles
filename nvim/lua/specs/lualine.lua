753be80215224f7870bf30c211a588a6e0e16981
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-18 21:25:07 +0530

    nvim: simplify lsp progress

--- nvim/lua/specs/lualine.lua
+++ nvim/lua/specs/lualine.lua
@@ -9,11 +9,11 @@ return {
         },
         sections = {
             lualine_a = { 'mode' },
-            lualine_b = {},
-            lualine_c = { 'filename', 'lsp_progress' },
+            lualine_b = { {'lsp_progress', display_components = {'lsp_client_name', {'percentage'}}}},
+            lualine_c = { 'filename' },
             lualine_x = { 'diagnostics' },
             lualine_y = { 'branch' },
-            lualine_z =      { 'location' }
+            lualine_z = { 'location' }
         },
     }
 }
