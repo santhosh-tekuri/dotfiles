099f2ebcbc629fae1dab5c1ccd19a3e41d8884cc
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-18 21:01:27 +0530

    nvm/lualine: use opts

--- nvim/lua/specs/lualine.lua
+++ nvim/lua/specs/lualine.lua
@@ -1,24 +1,20 @@
-local spec = { 
+return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "arkav/lualine-lsp-progress" },
-}
-
-function spec.config()
-    require("lualine").setup {
+    opts = {
         options = {
             icons_enabled = false,
-            component_separators = { left = ' ', right = ' '},
-            section_separators = { left = ' ', right = ' '},
+            component_separators = { left = ' ', right = ' ' },
+            section_separators = { left = ' ', right = ' ' },
         },
         sections = {
-            lualine_a = {'mode'},
+            lualine_a = { 'mode' },
             lualine_b = {},
-            lualine_c = {'filename', 'lsp_progress'},
-            lualine_x = {'diagnostics'},
-            lualine_y = {'branch'},
-            lualine_z = {'location'}
+            lualine_c = { 'filename', 'lsp_progress' },
+            lualine_x = { 'diagnostics' },
+            lualine_y = { 'branch' },
+            lualine_z =      { 'location' }
         },
     }
-end
+}
 
-return spec
