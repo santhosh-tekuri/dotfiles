5be785fcff155788c121e32be804bca2ba38578f
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-26 15:07:38 +0530

    nvim: use telescope codeactions

--- nvim/lua/specs/fzf.lua
+++ nvim/lua/specs/fzf.lua
@@ -25,8 +25,8 @@ function spec.config()
         callback = function(ev)
             vim.keymap.set('n', ' s', fzf.lsp_document_symbols, { desc = "Open symbol picker" })
             vim.keymap.set('n', ' S', fzf.lsp_workspace_symbols, { desc = "Open workspace symbol picker" })
-            vim.keymap.set({ 'n', 'v'} , ' a', fzf.lsp_code_actions, { desc = "Perform code action" })
-      end,
+            -- vim.keymap.set({ 'n', 'v'} , ' a', fzf.lsp_code_actions, { desc = "Perform code action" })
+        end,
     })
 end
 
--- nvim/lua/specs/telescope.lua
+++ nvim/lua/specs/telescope.lua
@@ -1,28 +1,36 @@
 local spec = {
-    "nvim-telescope/telescope.nvim", tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
 }
 
 function spec.config()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', ' f', builtin.find_files, { desc = "Open file picker" })
-    vim.keymap.set('n', ' b', builtin.buffers, { desc = "Open buffer picker" })
-    vim.keymap.set('n', ' /', builtin.live_grep, { desc = "Global search in workspace folder" })
+    -- vim.keymap.set('n', ' f', builtin.find_files, { desc = "Open file picker" })
+    -- vim.keymap.set('n', ' b', builtin.buffers, { desc = "Open buffer picker" })
+    -- vim.keymap.set('n', ' /', builtin.live_grep, { desc = "Global search in workspace folder" })
 
     -- when LS attaches to the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('TelescopeLspConfig', {}),
         callback = function(ev)
-            vim.keymap.set('n', 'gr', builtin.lsp_references, { desc = "Goto references" })
-            vim.keymap.set('n', ' s', builtin.lsp_document_symbols, { desc = "Open symbol picker" })
-            vim.keymap.set('n', ' S', builtin.lsp_workspace_symbols, { desc = "Open workspace symbol picker" })
-            vim.keymap.set('n', ' d', function() builtin.diagnostics({ bufnr = 0 }) end, { desc = "Open diagnostic picker" })
-            vim.keymap.set('n', ' D', builtin.diagnostics, { desc = "Open workspace diagnostic picker" })
-      end,
+            -- vim.keymap.set('n', ' s', builtin.lsp_document_symbols, { desc = "Open symbol picker" })
+            -- vim.keymap.set('n', ' S', builtin.lsp_workspace_symbols, { desc = "Open workspace symbol picker" })
+            -- vim.keymap.set({ 'n', 'v' }, ' a', builtin.lsp_code_actions, { desc = "Perform code action" })
+            vim.keymap.set({ 'n', 'v' }, ' a', vim.lsp.buf.code_action, { desc = "Perform code action" })
+        end,
     });
 
     local actions = require("telescope.actions")
     require("telescope").setup({
+        extensions = {
+            ["ui-select"] = {
+                require("telescope.themes").get_dropdown {},
+            },
+        },
         defaults = {
             mappings = {
                 i = {
@@ -31,6 +39,7 @@ function spec.config()
             },
         },
     })
+    require("telescope").load_extension("ui-select")
 end
 
 return spec
