5dc4f5204957d6dee5fa82245048b063e6f7857c
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-28 21:37:01 +0530

    nvim: split below/right

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,14 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true -- enable RGB colors
-vim.opt.expandtab = true     -- convert tabs to spaces
-vim.opt.shiftwidth = 4       -- no of spaces inserted for each indentation
-vim.opt.tabstop = 4          -- no of spaces inserted for tab
-vim.opt.signcolumn = "yes"   -- always show signcolumn
+vim.opt.number = true         -- show line number for each line
+vim.opt.relativenumber = true -- show relative line number for each line
+vim.opt.termguicolors = true  -- enable RGB colors
+vim.opt.expandtab = true      -- convert tabs to spaces
+vim.opt.shiftwidth = 4        -- no of spaces inserted for each indentation
+vim.opt.tabstop = 4           -- no of spaces inserted for tab
+vim.opt.signcolumn = "yes"    -- always show signcolumn
 vim.opt.wrap = false
-vim.opt.laststatus = 3       -- single statusline for all splits
+vim.opt.laststatus = 3        -- single statusline for all splits
+vim.opt.splitbelow = true     -- put new window below the split
+vim.opt.splitright = true     -- put new window right to the split
 
 -- Case-insensitive searching UNLESS \C or capital in search
 vim.opt.ignorecase = true
