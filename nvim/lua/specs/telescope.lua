bf20b78a0c325b8f53d76976a02d9e1daaac9b68
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-01-30 11:19:30 +0530

    nvim: use telescope

--- nvim/lua/specs/telescope.lua
+++ nvim/lua/specs/telescope.lua
@@ -9,19 +9,33 @@ local spec = {
 
 function spec.config()
     local builtin = require('telescope.builtin')
-    -- vim.keymap.set('n', ' f', builtin.find_files, { desc = "Open file picker" })
-    -- vim.keymap.set('n', ' b', builtin.buffers, { desc = "Open buffer picker" })
-    -- vim.keymap.set('n', ' /', builtin.live_grep, { desc = "Global search in workspace folder" })
+    vim.keymap.set('n', ' f', builtin.find_files, { desc = "Open file picker" })
+    vim.keymap.set('n', ' b', builtin.buffers, { desc = "Open buffer picker" })
+    vim.keymap.set('n', ' /', builtin.live_grep, { desc = "Global search in workspace folder" })
 
     -- when LS attaches to the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('TelescopeLspConfig', {}),
         callback = function(ev)
-            -- vim.keymap.set('n', ' s', builtin.lsp_document_symbols, { desc = "Open symbol picker" })
-            -- vim.keymap.set('n', ' S', builtin.lsp_workspace_symbols, { desc = "Open workspace symbol picker" })
-            -- vim.keymap.set({ 'n', 'v' }, ' a', vim.lsp.buf.code_action, { desc = "Perform code action" })
+            vim.keymap.set('n', ' s', builtin.lsp_document_symbols, { desc = "Open symbol picker" })
+            vim.keymap.set('n', ' S', builtin.lsp_workspace_symbols, { desc = "Open workspace symbol picker" })
+            vim.keymap.set({ 'n', 'v' }, ' a', vim.lsp.buf.code_action, { desc = "Perform code action" })
         end,
     });
+
+    -- horizontal_fused layout strategy
+    local layout_strategies = require("telescope.pickers.layout_strategies")
+    layout_strategies.horizontal_fused = function(picker, max_columns, max_lines, layout_config)
+        local layout = layout_strategies.horizontal(picker, max_columns, max_lines, layout_config)
+        layout.prompt.title = ""
+        layout.results.title = ""
+        layout.preview.title = ""
+        layout.results.height = layout.results.height + 1
+        layout.results.borderchars = { "─", "│", "─", "│", "╭", "┬", "┤", "├" }
+        layout.preview.borderchars = { "─", "│", "─", " ", "─", "╮", "╯", "─" }
+        layout.prompt.borderchars = { "─", "│", "─", "│", "╭", "╮", "┴", "╰" }
+        return layout
+    end
 
     local actions = require("telescope.actions")
     require("telescope").setup({
@@ -31,6 +45,7 @@ function spec.config()
             },
         },
         defaults = {
+            layout_strategy = "horizontal_fused",
             mappings = {
                 i = {
                     ["<esc>"] = actions.close, -- close telescope with single <esc>
