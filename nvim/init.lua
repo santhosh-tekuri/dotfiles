af3a9d17488640e7224782aff331fd775d682fe5
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-02-01 16:46:22 +0530

    nvim: add close_floats func

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,7 +26,15 @@ vim.keymap.set('n', ' w', '<c-w>', { remap = false })
 vim.keymap.set('v', '>', '>gv', { remap = false })
 vim.keymap.set('v', '<', '<gv', { remap = false })
 
-vim.keymap.set('n', '<esc>', '<c-w>o', { desc = "close other windows" })
+local function close_floats()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local config = vim.api.nvim_win_get_config(win)
+        if config.relative ~= "" then          -- is_floating_window?
+            vim.api.nvim_win_close(win, false) -- do not force
+        end
+    end
+end
+vim.keymap.set('n', '<esc>', close_floats, { desc = "close floating windows" })
 
 -- system clipboard
 vim.keymap.set({ 'n', 'v' }, ' y', '"+y', { desc = "Copy to system clipboard" })
