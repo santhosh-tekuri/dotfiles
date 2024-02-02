b94446895f1ed6ec5c932a9193388f9b71803c47
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-02-02 14:50:14 +0530

    nvim: disable virtualedit

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,11 +5,10 @@ vim.opt.expandtab = true      -- convert tabs to spaces
 vim.opt.shiftwidth = 4        -- no of spaces inserted for each indentation
 vim.opt.tabstop = 4           -- no of spaces inserted for tab
 vim.opt.signcolumn = "yes"    -- always show signcolumn
-vim.opt.wrap = false
+vim.opt.wrap = false          -- disable line wrap
 vim.opt.laststatus = 3        -- single statusline for all splits
 vim.opt.splitbelow = true     -- put new window below the split
 vim.opt.splitright = true     -- put new window right to the split
-vim.opt.virtualedit = "block" -- allow cursor anywhere in visual block mode
 vim.opt.inccommand = "split"  -- show substitute preview in split
 
 -- Case-insensitive searching UNLESS \C or capital in search
