7950018b68b8a5eb35b5f5493792a516f2493470
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-02-08 13:16:59 +0530

    nvim/lspconfig: configure each server manually

--- nvim/lua/specs/lspconfig.lua
+++ nvim/lua/specs/lspconfig.lua
@@ -19,53 +19,59 @@ function spec.config()
     local cap2 = require("cmp_nvim_lsp").default_capabilities(cap1)
     local capabilities = vim.tbl_deep_extend("force", cap1, cap2)
 
-    local on_attach = function(client, bufnr)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Goto definition" })
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Goto declaration" })
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Goto implementation" })
-        vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { desc = "Goto type definition" })
-        vim.keymap.set('n', ' r', vim.lsp.buf.rename, { desc = "Rename symbol" })
-        vim.keymap.set('n', ' k', vim.lsp.buf.hover, { desc = "Show docs for item under cursor" })
-        vim.keymap.set({ 'n', 'i' }, '<c-k>', vim.lsp.buf.signature_help, { desc = "Show signature" })
-        vim.keymap.set('n', ' e', vim.diagnostic.open_float, { desc = "Show error on current line" })
+    vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+            local function opts(desc)
+                return { buffer = ev.buf, desc = desc }
+            end
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts("Goto definition"))
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts("Goto declaration"))
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts("Goto implementation"))
+            vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts("Goto type definition"))
+            vim.keymap.set('n', ' r', vim.lsp.buf.rename, opts("Rename symbol"))
+            vim.keymap.set('n', ' k', vim.lsp.buf.hover, opts("Show docs for item under cursor"))
+            vim.keymap.set({ 'n', 'i' }, '<c-k>', vim.lsp.buf.signature_help, opts("Show signature"))
+            vim.keymap.set('n', ' e', vim.diagnostic.open_float, opts("Show error on current line"))
 
-        if client.supports_method("textDocument/formatting") then
-            local group = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
-            vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = group,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format()
-                end,
-            })
-        end
-    end
+            local client = vim.lsp.get_client_by_id(ev.data.client_id)
+            if client.supports_method("textDocument/formatting") then
+                local group = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
+                vim.api.nvim_clear_autocmds({ group = group, buffer = ev.buf })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = group,
+                    buffer = ev.buf,
+                    callback = function()
+                        vim.lsp.buf.format()
+                    end,
+                })
+            end
+        end,
+    })
 
+    local servers = {
+        "lua_ls",
+        "gopls",
+        "rust_analyzer",
+        "pyright",
+    }
     -- install serve
     require("mason").setup()
     require("neodev").setup({})
     require("mason-lspconfig").setup {
-        ensure_installed = {
-            "lua_ls",
-            "gopls",
-            "rust_analyzer",
-            "pyright",
-        },
-        handlers = {
-            function(server_name)
-                local config = {
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                }
-                local ok, settings = pcall(require, "lspsettings." .. server_name)
-                if ok then
-                    config.settings = settings
-                end
-                require("lspconfig")[server_name].setup(config)
-            end,
-        },
+        ensure_installed = servers,
     }
+
+    for _, server in ipairs(servers) do
+        local config = {
+            capabilities = capabilities,
+        }
+        local ok, settings = pcall(require, "lspsettings." .. server)
+        if ok then
+            config.settings = settings
+        end
+        require("lspconfig")[server].setup(config)
+    end
 
     -- disable virtual text for diagnostics
     vim.diagnostic.config({ virtual_text = false })
