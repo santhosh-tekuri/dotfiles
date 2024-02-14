1a8de1772bc46a1036eaebe900b10c322c7b22b9
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-02-14 20:13:58 +0530

    nvim/cmp: disable arrow keys

--- nvim/lua/specs/cmp.lua
+++ nvim/lua/specs/cmp.lua
@@ -74,6 +74,10 @@ function spec.config()
             end,
         },
         mapping = cmp.mapping.preset.insert({
+            -- disable arrow keys
+            ['<Down>'] = function() end,
+            ['<Up>'] = function() end,
+
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
