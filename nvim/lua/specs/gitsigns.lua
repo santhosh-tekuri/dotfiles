e56325fadc0eea3099e0112be39513200b94e125
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-03-22 16:32:45 +0530

    nvim/gitsigns: set priority

--- nvim/lua/specs/gitsigns.lua
+++ nvim/lua/specs/gitsigns.lua
@@ -1,7 +1,9 @@
 local spec = { "lewis6991/gitsigns.nvim" }
 
 function spec.config()
-    require("gitsigns").setup()
+    require("gitsigns").setup({
+        sign_priority = 100, -- priority for sign over diagnostic
+    })
 end
 
 return spec
