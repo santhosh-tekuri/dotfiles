385575be0cd125d59e94ad63ac28fa08bd113f44
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-03-31 10:48:22 +0530

    nvim/autopair: disable ' autopair for rust

--- nvim/lua/specs/autopairs.lua
+++ nvim/lua/specs/autopairs.lua
@@ -1,7 +1,23 @@
 local spec = { "altermo/ultimate-autopair.nvim" }
 
 function spec.config()
-    require("ultimate-autopair").setup()
+    require("ultimate-autopair").setup({
+        config_internal_pairs = {
+            -- disable ' autopair for rust
+            {
+                "'",
+                "'",
+                multiline = false,
+                surround = true,
+                cond = function(fn)
+                    if fn.get_ft() == 'rust' then
+                        return false
+                    end
+                    return not fn.in_lisp() or fn.in_string()
+                end,
+            },
+        },
+    })
 end
 
 return spec
