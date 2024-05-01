2959810ef8f5e85bca91151c65c022d591a95c32
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-05-01 14:35:54 +0530

    nvim: show confirm dialog

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -13,6 +13,7 @@ vim.opt.inccommand = "split"  -- show substitute preview in split
 vim.opt.ignorecase = true     -- prefer case-insensitive search UNLESS \C prefix
 vim.opt.smartcase = true      -- override ignorecase when pattern has uppercase
 vim.opt.scrolloff = 3         -- min lines to keep above/below cursor
+vim.opt.confirm = true        -- show dialog for confirmation
 
 -- show whitespace using :set list
 vim.opt.listchars = { space = '.', tab = '>-', trail = '.' }
