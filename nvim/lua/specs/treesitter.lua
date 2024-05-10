0aee38a0851f0c2622c37dcafaff655d9cac6046
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-05-10 17:31:33 +0530

    nvim/treesitter: add tag

--- nvim/lua/specs/treesitter.lua
+++ nvim/lua/specs/treesitter.lua
@@ -1,5 +1,7 @@
 local spec = {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.2",
+    build = ":TSUpdate",
 }
 
 function spec.config()
