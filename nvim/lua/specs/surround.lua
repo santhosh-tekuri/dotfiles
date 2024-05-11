5e244282c2a671fa068ef4b2c69fcfdd7044952b
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-05-11 17:09:31 +0530

    nvim/surround: add doc

--- nvim/lua/specs/surround.lua
+++ nvim/lua/specs/surround.lua
@@ -4,3 +4,20 @@ return {
     event = "VeryLazy",
     opts = {},
 }
+
+--[[
+ys{motion}{char}        surround by {char}
+cs{char}{replacement}   replace surrouding {char} by {replacement}
+ds{char}                delete surrounding {char}
+
+use `s` for {char} to find surrounding char automatically
+
+S{char}                 surround selection with {char} in visual mode
+<c-g>s{char}            surrount cursor with {char} in insert mode
+
+Aliases:
+b   represents )
+B   represents }
+r   represents ]
+q   represents single/double/back quotes
+--]]
