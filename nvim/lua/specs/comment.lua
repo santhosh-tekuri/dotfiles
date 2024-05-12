fcf14633ba06ce33774afb96cd1daf460ad03253
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-05-12 14:44:01 +0530

    nvim/comment: context aware commentstring

--- nvim/lua/specs/comment.lua
+++ nvim/lua/specs/comment.lua
@@ -1,7 +1,17 @@
-return {
+local spec = {
     "numToStr/Comment.nvim",
-    opts = {}
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring"
+    }
 }
+
+function spec.config()
+    require("Comment").setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
+    }
+end
+
+return spec
 
 --[[
 gc{noun}    toggle line comment
