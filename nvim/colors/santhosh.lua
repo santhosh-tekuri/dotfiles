387293926f94dccddfd2b936fe64fd9bbfe9cfb0
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2024-09-04 20:30:33 +0530

    nvim: colorscheme santhosh

--- nvim/colors/santhosh.lua
+++ nvim/colors/santhosh.lua
@@ -6,36 +6,39 @@ for _, group in ipairs(vim.fn.getcompletion("@lsp.", "highlight")) do
     vim.api.nvim_set_hl(0, group, {})
 end
 
-local white = "#9ca1a8"
+local black = "#0d0f18"
+local white = "#a5b6cf"
+local red = "#dd6777"
+local green = "#90ceaa"
+
 local groups = {
     Default = { fg = white },
-    Normal = { bg = "#000000", fg = white },
-    String = { fg = "#90b99f" },
-    -- String = { fg = "#678a45" },
+    Normal = { bg = black, fg = white },
+    String = { fg = green },
     -- Operator = { fg = "#e6b99d" },
     Operator = { fg = "#ae8e57" },
     Type = { link = "Default" },
-    Comment = { fg = "#4c5063" },
-    Visual = { bg = "#1e1e1e" },
+    Comment = { fg = "#747d8c", italic = true },
+    Visual = { bg = "#313131" },
     Constant = { link = "String" },
     Function = { link = "Default" },
     Identifier = { link = "Default" },
-    DiagnosticError = { fg = "#ac4143" },
+    DiagnosticError = { fg = red },
     NormalFloat = { bg = "#121213" },
     PMenu = { link = "NormalFloat" },
-    PmenuSel = { bg = white, fg = "#000000" },
+    PmenuSel = { bg = white, fg = black },
     CmpItemKind = { link = "Comment" },
     MatchParen = { bg = "#4c5063", fg = "#FFFFFF" },
-    GitSignsAdd = { fg = "#345b39" },
+    GitSignsAdd = { fg = green },
     GitSignsChange = { fg = "#946a37" },
-    GitSignsDelete = { fg = "#ac4143" },
+    GitSignsDelete = { fg = red },
     -- FloatBorder = { fg = "#000000" },
     -- TelescopeNormal = { link = "NormalFloat" },
     -- TelescopeBorder = { link = "FloatBorder" },
     ["@constructor"] = { link = "Default" },
     ["@lsp.type.namespace.go"] = { link = "String" },
     ["@variable"] = { link = "Default" },
-    ["@keyword"] = { fg = "#808ebe" },
+    ["@keyword"] = { fg = "#6699cc" },
     -- ["@keyword"] = { fg = "#6388c4" },
     ["@type.builtin"] = { link = "@keyword" },
     ["@variable.builtin"] = { link = "@keyword" },
