109ff78c5b2651b2f6c8468d311bf757ce462809
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-04-18 19:14:24 +0530

    nvim/insjump: fix cursor outside buffer

--- nvim/lua/insjump.lua
+++ nvim/lua/insjump.lua
@@ -1,12 +1,9 @@
 -- use <C-L> in insert mode to jump to end of current treesitter node
 
 vim.keymap.set("i", "<C-L>", function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    line = line - 1
     local node = vim.treesitter.get_node()
     if node ~= nil then
-        local row
-        row, col = node:end_()
-        vim.api.nvim_win_set_cursor(0, { row + 1, col })
+        local row, col = node:end_()
+        pcall(vim.api.nvim_win_set_cursor, 0, { row + 1, col })
     end
 end, { desc = "insjump" })
