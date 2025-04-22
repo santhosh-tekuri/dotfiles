435422e9d7e172ac2c99f5171acb36999d8312e4
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-04-23 01:13:46 +0530

    nvim/lsp: install lsp using MasonInstall

--- nvim/lua/specs/lsp.lua
+++ nvim/lua/specs/lsp.lua
@@ -4,41 +4,16 @@ local spec = {
     "williamboman/mason.nvim",
 }
 
-local function ensure_installed(pkg_name)
-    local registry = require("mason-registry")
-    local ok, pkg = pcall(registry.get_package, pkg_name)
-    if not ok then
-        vim.notify(("no mason package: %s"):format(pkg_name), vim.log.levels.ERROR)
-        return
-    end
-    if pkg:is_installed() then
-        return
-    end
-    pkg:install({ version = nil }):once(
-        'closed',
-        vim.schedule_wrap(function()
-            if pkg:is_installed() then
-                vim.notify(("[mason] %s was successfully installed"):format(pkg_name))
-            else
-                vim.notify(
-                    ("[mason] %s install failed. see :Mason and :MasonLog"):format(pkg_name),
-                    vim.log.levels.ERROR
-                )
-            end
-        end)
-    )
-end
-
 function spec.config()
     require("mason").setup()
-    local servers = {}
     for _, f in pairs(vim.api.nvim_get_runtime_file('lsp/*.lua', true)) do
-        table.insert(servers, vim.fn.fnamemodify(f, ':t:r'))
+        local name = vim.fn.fnamemodify(f, ':t:r')
+        local cmd = dofile(f).cmd[1]
+        if vim.fn.executable(cmd) == 0 then
+            vim.cmd("MasonInstall " .. name)
+        end
+        vim.lsp.enable(name)
     end
-    for _, server in ipairs(servers) do
-        ensure_installed(server)
-    end
-    vim.lsp.enable(servers)
 end
 
 return spec
