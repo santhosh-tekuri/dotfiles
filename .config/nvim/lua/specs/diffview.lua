d384032b0d43b08735e0bf55d264e516e1133ce8
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-05-05 20:11:38 +0530

    nvim: rm diffview.lua

--- .config/nvim/lua/specs/diffview.lua
+++
@@ -1,9 +0,0 @@
--- single tabpage interface for easily cycling through
--- diffs for all modified files for any git rev
-
-return {
-    "sindrets/diffview.nvim",
-    opts = {
-        use_icons = false,
-    },
-}
