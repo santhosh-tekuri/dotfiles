fbf18fe0becbecc43173a57b074696a429591cf3
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-05-08 17:58:51 +0530

    install: make idempotent

--- install.sh
+++ install.sh
@@ -1,24 +1,51 @@
 #!/usr/bin/env bash
 
-echo source $PWD/.bashrc >> ~/.bashrc
-echo source $PWD/.zshrc >> ~/.zshrc
-echo source $PWD/.vimrc >> ~/.vimrc
-echo source $PWD/.screenrc >> ~/.screenrc
+function addline() {
+    if [ -f "$2" ]; then
+        grep -qxF "$1" "$2" || echo "$2" >> "$1"
+    else
+        echo "$2" >> "$1"
+    fi
+}
+
+addline "source $PWD/.bashrc" ~/.bashrc
+addline "source $PWD/.zshrc" ~/.zshrc
+addline "source $PWD/.vimrc" ~/.vimrc
+addline "source $PWD/.screenrc" ~/.screenrc
 mkdir -p ~/.ssh
-echo Include $PWD/.ssh/config >> ~/.ssh/config
+addline "Include $PWD/.ssh/config" ~/.ssh/config
+
+function link() {
+    local src=$1
+    local tgt=$2
+    local tgtfile=$tgt/$(basename $src)
+    if [ -e $tgtfile ]; then
+        if [ $(readlink $tgtfile) == $src ]; then
+            return 0
+        fi
+    fi
+    ln -s $src $tgt
+}
 
 # link .config subfolders
 mkdir -p ~/.config
 for dir in $PWD/.config/*; do
-    ln -s $dir ~/.config
+    link $dir ~/.config
 done
+
+function gitclone() {
+    local url=$1
+    local last=${url##*/}
+    local name=${last%%.git}
+    [ ! -d $name ] && git clone $url
+}
 
 # install zsh plugins
 mkdir -p ~/.local/share/zsh
 cd ~/.local/share/zsh
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
-git clone https://github.com/zsh-users/zsh-history-substring-search.git
-git clone https://github.com/Aloxaf/fzf-tab
-cd -
+gitclone https://github.com/zdharma-continuum/fast-syntax-highlighting.git
+gitclone https://github.com/zsh-users/zsh-autosuggestions.git
+gitclone https://github.com/zsh-users/zsh-history-substring-search.git
+gitclone https://github.com/Aloxaf/fzf-tab.git
+cd - > /dev/null
 
