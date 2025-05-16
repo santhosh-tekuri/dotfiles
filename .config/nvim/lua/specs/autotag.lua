3c950846c60d4ac57f6472fc499b626a60e5f3bb
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-05-16 23:12:26 +0530

    nvim/autotag: add treesitter dep

--- .config/nvim/lua/specs/autotag.lua
+++ .config/nvim/lua/specs/autotag.lua
@@ -1,6 +1,11 @@
 -- automatically close and rename html tag
 
-local spec = { "windwp/nvim-ts-autotag" }
+local spec = {
+    "windwp/nvim-ts-autotag",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+    }
+}
 
 function spec.config()
     require("nvim-treesitter.configs").setup {
