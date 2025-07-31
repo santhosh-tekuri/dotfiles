d4f300f570fc35c56960d46271c0abd721ac511c
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-07-31 23:31:33 +0530

    nvim: 'q' binging must be set local to quicklist

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -3,6 +3,4 @@ vim.opt.relativenumber = false
 vim.opt.signcolumn = "no"
 vim.o.cursorline = true
 
-vim.keymap.set('n', 'q', function()
-    vim.cmd('q')
-end)
+vim.keymap.set('n', 'q', ':q<cr>', { buffer = 0 })
