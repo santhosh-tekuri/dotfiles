b7fb96a996f1da5c46171a5059a76fe2c13f136b
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-08-09 17:27:01 +0530

    nvim: add SudoWrite command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,1 +1,8 @@
+vim.api.nvim_create_user_command('SudoWrite', function()
+    local tmp = vim.fn.tempname()
+    vim.cmd('write! ' .. tmp)
+    vim.cmd('terminal sudo tee % < ' .. tmp .. ' > /dev/null')
+    vim.cmd('e!')
+end, {})
+
 vim.api.nvim_create_user_command("W", "noautocmd w", {})
