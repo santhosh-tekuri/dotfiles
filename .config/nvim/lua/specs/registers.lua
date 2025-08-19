a1ce86c1a4ca4622b5e82810501c7c95ee0683b2
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-08-19 17:55:42 +0530

    nvim/registers: show num first, hide empty

--- .config/nvim/lua/specs/registers.lua
+++ .config/nvim/lua/specs/registers.lua
@@ -2,6 +2,8 @@ local spec = { "tversteeg/registers.nvim" }
 
 function spec.config()
     require("registers").setup {
+        show = "0123456789abcdefghijklmnopqrstuvwxyz*+\"-/_=#%.:",
+        show_empty = false,
         show_register_types = false,
         window = {
             transparency = 0,
