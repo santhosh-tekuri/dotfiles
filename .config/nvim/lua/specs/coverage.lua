934b58509fbd502eed61a225c86c7bd85d51f232
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-08-24 19:53:51 +0530

    nvim: update fmt

--- .config/nvim/lua/specs/coverage.lua
+++ .config/nvim/lua/specs/coverage.lua
@@ -1,7 +1,3 @@
 -- displays coverage information in the sign column
 
-return {
-    "andythigpen/nvim-coverage",
-    cmd = "Coverage",
-    opts = {},
-}
+return { "andythigpen/nvim-coverage", cmd = "Coverage", opts = {} }
