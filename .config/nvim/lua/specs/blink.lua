07ffd04ee8d8ac1287f9e0257da493568ba7dff6
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-08-27 17:17:05 +0530

    nvim/blink/cmdline: enter accept_enter

--- .config/nvim/lua/specs/blink.lua
+++ .config/nvim/lua/specs/blink.lua
@@ -49,6 +49,7 @@ function spec.config()
                 preset = 'super-tab',
                 ['<C-p>'] = { 'select_prev', 'fallback' },
                 ['<C-n>'] = { 'select_next', 'fallback' },
+                ['<enter>'] = { 'select_accept_and_enter', "fallback" },
 
             }
         }
