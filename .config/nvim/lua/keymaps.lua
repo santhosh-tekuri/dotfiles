9b82df554cfa24f429fa0f97d7153f1358c22bb5
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-09-04 20:54:19 +0530

    nvim/picker: use leader+r

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -67,7 +67,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end
         vim.keymap.set({ 'n', 'v' }, ' a', vim.lsp.buf.code_action, opts("Perform code action"))
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts("Goto declaration"))
-        vim.keymap.set('n', ' r', vim.lsp.buf.rename, opts("Rename symbol"))
+        vim.keymap.set('n', ' n', vim.lsp.buf.rename, opts("Rename symbol"))
         vim.keymap.set({ 'n', 'v' }, 'K', function()
             vim.lsp.buf.hover { max_width = 80 }
         end, opts("Show docs for item under cursor"))
