c4526f2a08c464c146e315312a740b0938b9b3b5
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2025-09-28 00:58:12 +0530

    nvim: add Messages cmd

--- .config/nvim/lua/usercmds.lua
+++ .config/nvim/lua/usercmds.lua
@@ -1,3 +1,25 @@
+vim.api.nvim_create_user_command('Messages', function()
+    local messages = vim.fn.execute("messages", "silent")
+    if messages:sub(1, 1) == '\n' then
+        messages = messages:sub(2)
+    end
+    if #messages == 0 then
+        return
+    end
+    local lines = vim.split(messages, "\n")
+    if #lines > 0 then
+        local buf = vim.api.nvim_create_buf(false, true)
+        vim.b[buf].wipe = true
+        vim.bo[buf].bufhidden = 'wipe'
+        vim.api.nvim_buf_call(buf, function()
+            vim.cmd("file [Messages]")
+        end)
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+        local cwin = vim.api.nvim_get_current_win()
+        vim.api.nvim_win_set_buf(cwin, buf)
+    end
+end, {})
+
 vim.api.nvim_create_user_command('SudoWrite', function()
     local tmp = vim.fn.tempname()
     vim.cmd('write! ' .. tmp)
